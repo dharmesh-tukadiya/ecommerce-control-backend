8cd3867e94600a0882d68a625b804c93da0f0b8b
dharmesh-tukadiya <dharmeshntukadiya@gmail.com>
2025-10-01 17:25:55 +0000

    first commit

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\FortifyServiceProvider::class,
+    // https://tenancyforlaravel.com/docs/v3/installation
+    App\Providers\TenancyServiceProvider::class,
+];
